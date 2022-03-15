0b9bf3da4fc304fa0a7c471ae6bd3499e9b737f1
autJiahao <tss4432@aut.ac.nz>
2022-03-16 04:24:15 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const articles = require("./dummydata/articles");
+
+const server = express();
+
+server.get('/', (req,res) => {res.send("API is running")});
+
+server.get('/api/articles', (req,res) => {
+    res.json(articles);
+});
+
+server.get('/api/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(articles);
+    console.log(req.params);
+});
+
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT||5000;
+
+server.listen(PORT,console.log(`server is working and listening on PORT ${PORT}`));
+//server.listen(5000,console.log("Server is working and listening PORT 5000"));
